2c07f77066bc15ace439f8cc7eba9753e7137fe4
Camila Martinez <camilamarb98@gmail.com>
2022-05-25 20:04:13 -0500

    organize folders

---
+++ chapter_1/hello_world/main.rs
@@ -0,0 +1,5 @@
+fn main(){ // main function: first code that runs in every executable Rust program
+    println!("Hello world!");   // Calls a Rust macro
+}
+
+// Compile it with 'rustc main.rs' to create executable
