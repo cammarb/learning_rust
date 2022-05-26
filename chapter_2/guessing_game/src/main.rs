09cb95ecd441fc34304d0bc12349bbc91f27b6ed
Camila Martinez <camilamarb98@gmail.com>
2022-05-25 22:03:02 -0500

    add user input

--- chapter_2/guessing_game/src/main.rs
+++ chapter_2/guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io; //io - input/output library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    // mut makes the variable mutable.
+    // The :: syntax indicates that new is an associated function of String
+    let mut guess = String::new();
+    io::stdin()
+        // .read_line() method of the stdin library
+        // The & indicates that this argument is a reference
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
