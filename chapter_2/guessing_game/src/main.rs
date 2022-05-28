ef56002cda500085e30d4dd59e4cdf21888cce41
Camila Martinez <camilamarb98@gmail.com>
2022-05-27 19:27:28 -0500

    add loop, error handling

--- chapter_2/guessing_game/src/main.rs
+++ chapter_2/guessing_game/src/main.rs
@@ -7,27 +7,33 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
+    // Line just for testing
+    // println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
-    // mut makes the variable mutable.
-    // The :: syntax indicates that new is an associated function of String
-    let mut guess = String::new();
-    io::stdin()
-        // .read_line() method of the stdin library
-        // The & indicates that this argument is a reference
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-
-    // NOTE: crate is a collection of Rust source code files
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Please input your guess.");
+        // mut makes the variable mutable.
+        // The :: syntax indicates that new is an associated function of String
+        let mut guess = String::new();
+        io::stdin()
+            // .read_line() method of the stdin library
+            // The & indicates that this argument is a reference
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+        // NOTE: crate is a collection of Rust source code files
+        // The cmp method compares two values and can be called on anything that can be compared
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
