2ca4992f3a3151f82ddea347cf08e7a14a27a07c
Camila Martinez <camilamarb98@gmail.com>
2022-05-31 21:14:34 -0500

    new project

---
+++ practice/temperature_converter/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    println!("Hello, welcome to temperature unit converter!");
+    println!("Press C to start converting from Celcius or F, to start converting from Faranheit:");
+
+    let mut temperature = String::new();
+
+    io::stdin().read_line()
+}
