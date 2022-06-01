077f72af70a46a913dc3ca44161f244405c960ee
Camila Martinez <camilamarb98@gmail.com>
2022-06-01 16:52:16 -0500

    update

--- practice/temperature_converter/src/main.rs
+++ practice/temperature_converter/src/main.rs
@@ -1,10 +1,36 @@
 use std::io;
+use std::cmp::Ordering;
+
+fn to_celcius(temperature){
+
+}
+
+fn to_farenheit(temperature){
+
+}
 
 fn main() {
-    println!("Hello, welcome to temperature unit converter!");
-    println!("Press C to start converting from Celcius or F, to start converting from Faranheit:");
+    loop{ 
+        println!("Hello, welcome to temperature unit converter!");
+        println!("Press C to start converting from Celcius or F, to start converting from Faranheit:");
+        let mut option = String::new();
+        io::stdin()
+            .read_line(&mut option)
+            .expect("Failed to read the line.");
+        println!("Option selected: {}", option);
 
-    let mut temperature = String::new();
+        println!("Insert temperature: ");
+        let mut temperature = String::new();
+        io::stdin().read_line(&mut temperature).expect("Failed to read line.");
+        let temperature:u32= match temperature.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Temperature inserted: {}", temperature);
 
-    io::stdin().read_line()
+
+        match option{
+
+        }
+    }
 }
