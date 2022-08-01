57272eec4ac0c96a1a4288fe08c035cd0e56a9e2
Camila Martinez <camilamarb98@gmail.com>
2022-08-01 15:18:51 -0500

    add converter functions

--- practice/temperature_converter/src/main.rs
+++ practice/temperature_converter/src/main.rs
@@ -1,18 +1,27 @@
 use std::io;
-use std::cmp::Ordering;
 
-fn to_celcius(temperature){
-
+fn to_celcius(initial_temp: f32) {
+    let final_temp: f32 = (initial_temp - 32.0) * 0.5556;
+    println!(
+        "The temperature {} Farenheit is equal to {} Celcius.",
+        initial_temp, final_temp
+    );
 }
 
-fn to_farenheit(temperature){
-
+fn to_farenheit(initial_temp: f32) {
+    let final_temp: f32 = (1.8 * initial_temp) + 32.0;
+    println!(
+        "The temperature {} Celcius is equal to {} Farenheit.",
+        initial_temp, final_temp
+    );
 }
 
 fn main() {
-    loop{ 
+    loop {
         println!("Hello, welcome to temperature unit converter!");
-        println!("Press C to start converting from Celcius or F, to start converting from Faranheit:");
+        println!(
+            "Press C to start converting from Celcius or F, to start converting from Faranheit:"
+        );
         let mut option = String::new();
         io::stdin()
             .read_line(&mut option)
@@ -21,16 +30,20 @@ fn main() {
 
         println!("Insert temperature: ");
         let mut temperature = String::new();
-        io::stdin().read_line(&mut temperature).expect("Failed to read line.");
-        let temperature:u32= match temperature.trim().parse(){
+        io::stdin()
+            .read_line(&mut temperature)
+            .expect("Failed to read line.");
+        let temperature: f32 = match temperature.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
         println!("Temperature inserted: {}", temperature);
 
-
-        match option{
-
+        if option.contains('c') {
+            to_farenheit(temperature);
+        }
+        if option.contains('f') {
+            to_celcius(temperature);
         }
     }
 }
